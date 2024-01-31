c0a12258d94632c67103e8484ef67b6b2a909a33
Egor Makushkin <150073461+Opsik25@users.noreply.github.com>
2024-02-01 00:51:03 +0300

    Update Music_service_DB_creating.sql

--- Music_service_DB_creating.sql
+++ Music_service_DB_creating.sql
@@ -1,41 +1,50 @@
 CREATE TABLE music_genre (
-	genre_id SERIAL PRIMARY KEY,
+	PRIMARY KEY (genre_id),
+	genre_id   SERIAL 		NOT NULL,
 	genre_name VARCHAR(255) UNIQUE NOT NULL
 );
 CREATE TABLE musician (
-	musician_id SERIAL PRIMARY KEY,
+	PRIMARY KEY (musician_id),
+	musician_id   SERIAL 	   NOT NULL,
 	musician_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE genre_musician (
-	genre_id INT REFERENCES music_genre(genre_id),
-	musician_id INT REFERENCES musician(musician_id),
-	CONSTRAINT genre_musician_id PRIMARY KEY(genre_id, musician_id)
+	PRIMARY KEY (genre_id, musician_id),
+	genre_id    INT NOT NULL REFERENCES music_genre(genre_id),
+	musician_id INT NOT NULL REFERENCES musician(musician_id)	
 );
 CREATE TABLE album (
-	album_id SERIAL PRIMARY KEY,
-	album_name VARCHAR(255) NOT NULL,
-	release_year SMALLINT NOT NULL CHECK
-		(release_year>1900)
+	PRIMARY KEY (album_id),
+	album_id     SERIAL 	  NOT NULL,
+	album_name   VARCHAR(255) NOT NULL,
+	release_year SMALLINT 	  NOT NULL, 
+				 CONSTRAINT album_release_year_range
+				 CHECK (release_year BETWEEN 1900 AND 2100)
 );
 CREATE TABLE musician_album (
-	musician_id INT REFERENCES musician(musician_id),
-	album_id INT REFERENCES album(album_id),
-	CONSTRAINT musician_album_id PRIMARY KEY(musician_id, album_id)
+	PRIMARY KEY (musician_id, album_id),
+	musician_id INT NOT NULL REFERENCES musician(musician_id),
+	album_id 	INT NOT NULL REFERENCES album(album_id)			
 );
 CREATE TABLE song (
-	song_id SERIAL PRIMARY KEY,
+	PRIMARY KEY (song_id),
+	song_id   SERIAL 	   NOT NULL,
 	song_name VARCHAR(255) NOT NULL,
-	duration INTEGER NOT NULL,
-	album_id INT NOT NULL REFERENCES album(album_id) ON DELETE CASCADE
+	duration  INTEGER 	   NOT NULL,
+			  CONSTRAINT song_duration_check
+			  CHECK (duration > 0),
+	album_id  INT 		   NOT NULL REFERENCES album(album_id) ON DELETE CASCADE
 );
 CREATE TABLE collection (
-	collection_id SERIAL PRIMARY KEY,
+	PRIMARY KEY (collection_id),
+	collection_id   SERIAL 		 NOT NULL,
 	collection_name VARCHAR(255) NOT NULL,
-	release_year SMALLINT NOT NULL CHECK
-		(release_year>1900)
+	release_year 	SMALLINT 	 NOT NULL,
+					CONSTRAINT collection_release_year_range
+					CHECK (release_year BETWEEN 1900 AND 2100)
 );
 CREATE TABLE collection_song (
-	collection_id INT REFERENCES collection(collection_id),
-	song_id INT REFERENCES song(song_id),
-	CONSTRAINT collection_song_id PRIMARY KEY(collection_id, song_id)
+	PRIMARY KEY (collection_id, song_id),
+	collection_id INT NOT NULL REFERENCES collection(collection_id),
+	song_id 	  INT NOT NULL REFERENCES song(song_id)	  
 );
