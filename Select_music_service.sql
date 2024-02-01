d511539138fa854ef67b56ad4d893f3ee9a91667
Egor Makushkin <150073461+Opsik25@users.noreply.github.com>
2024-02-02 00:15:51 +0300

    Update Select_music_service.sql

--- Select_music_service.sql
+++ Select_music_service.sql
@@ -57,10 +57,9 @@ SELECT mg.genre_name,
 -- №2 Количество треков, вошедших в альбомы 2019–2020 годов.
 SELECT COUNT(*) AS count_of_songs
   FROM song AS s
- WHERE s.album_id IN 
-       (SELECT a.album_id 
-	  FROM album AS a 
-	 WHERE a.release_year IN (2019, 2020));
+  JOIN album AS a
+    ON s.album_id = a.album_id 
+ WHERE a.release_year IN (2019, 2020);
 
 -- №3 Средняя продолжительность треков по каждому альбому.
 -- Вариант 1
@@ -80,78 +79,69 @@ SELECT COUNT(*) AS count_of_songs
   
 
 -- №4 Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT m.musician_name 
+SELECT m.musician_name
   FROM musician AS m
  WHERE m.musician_id NOT IN 
-       (SELECT ma.musician_id 
-       FROM musician_album AS ma
-      WHERE ma.album_id = (SELECT a.album_id 
-			     FROM album AS a 
-			    WHERE a.release_year = 2020));
+       (SELECT ma.musician_id
+          FROM musician_album AS ma
+          JOIN album AS a
+            ON ma.album_id = a.album_id 
+         WHERE a.release_year = 2020);
 
--- №5 Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
-SELECT c.collection_name 
+-- №5 Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).												  
+SELECT DISTINCT c.collection_name 
   FROM collection AS c
- WHERE c.collection_id IN 
-       (SELECT cs.collection_id 
- 	  FROM collection_song AS cs
-	 WHERE cs.song_id IN 
-	       (SELECT s.song_id 
-		  FROM song AS s
-		 WHERE s.album_id IN 
-		       (SELECT ma.album_id 
-			  FROM musician_album AS ma
-			 WHERE ma.musician_id = (SELECT m.musician_id 
-						   FROM musician AS m
-						  WHERE m.musician_name = 'Madonna'))));
+  JOIN collection_song AS cs
+    ON c.collection_id = cs.collection_id
+  JOIN song AS s
+    ON cs.song_id = s.song_id
+  JOIN album AS a
+    ON s.album_id = a.album_id
+  JOIN musician_album AS ma
+    ON a.album_id = ma.album_id
+  JOIN musician AS m
+    ON ma.musician_id = m.musician_id
+ WHERE m.musician_name = 'Madonna';
 
 -- SELECT-запросы. Задание 4.
 
--- №1 Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT a.album_name 
+-- №1 Названия альбомов, в которых присутствуют исполнители более чем одного жанра.		
+SELECT a.album_name
   FROM album AS a
- WHERE a.album_id IN 
-       (SELECT ma.album_id 
-          FROM musician_album AS ma
-	 WHERE ma.musician_id IN 
-	       (SELECT gm.musician_id 
-		  FROM genre_musician AS gm
-		 GROUP BY gm.musician_id
-		HAVING COUNT(gm.musician_id) > 1));
+  JOIN musician_album AS ma
+    ON a.album_id = ma.album_id
+  JOIN genre_musician AS gm
+    ON ma.musician_id = gm.musician_id
+ GROUP BY a.album_name
+HAVING COUNT(gm.musician_id) > 1;
 
--- №2 Наименования треков, которые не входят в сборники.
-SELECT s.song_name 
+-- №2 Наименования треков, которые не входят в сборники.	  	 
+SELECT s.song_name
   FROM song AS s
- WHERE s.song_id NOT IN 
-       (SELECT cs.song_id 
-	  FROM collection_song AS cs);
+  LEFT JOIN collection_song AS cs
+    ON s.song_id = cs.song_id
+ WHERE cs.collection_id IS NULL;
 
---№3 Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
+--№3 Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.							  	 
 SELECT m.musician_name 
   FROM musician AS m
- WHERE m.musician_id IN 
-       (SELECT ma.musician_id 
- 	  FROM musician_album AS ma
-	 WHERE ma.album_id IN 
-	       (SELECT a.album_id 
-		  FROM album AS a
-		 WHERE a.album_id IN 
-		       (SELECT s1.album_id 
-			  FROM song AS s1
-			 WHERE s1.duration IN 
-			       (SELECT MIN(s2.duration) 
-				  FROM song AS s2))));
+  JOIN musician_album AS ma
+    ON m.musician_id = ma.musician_id 
+  JOIN album AS a
+    ON ma.album_id = a.album_id 
+  JOIN song AS s1
+    ON a.album_id = s1.album_id 
+ WHERE s1.duration IN
+       (SELECT MIN(s2.duration) 
+          FROM song AS s2);
 
 --№4 Названия альбомов, содержащих наименьшее количество треков.
-SELECT a.album_name 
+SELECT a.album_name
   FROM album AS a
- WHERE a.album_id IN 
-       (SELECT album_id 
- 	  FROM (SELECT s1.album_id, COUNT(s1.song_name) AS song_count 
- 	      	  FROM song AS s1
-		 GROUP BY s1.album_id
-		HAVING COUNT(s1.song_name) = (SELECT MIN(song_count) 
-						FROM (SELECT s2.album_id, COUNT(s2.song_name) AS song_count 
-							FROM song AS s2
-						       GROUP BY s2.album_id))))
-ORDER BY a.album_id;
+  JOIN song AS s
+    ON a.album_id = s.album_id
+ GROUP BY a.album_name
+HAVING COUNT(a.album_name) = (SELECT MIN(song_count) 
+                                FROM (SELECT COUNT(*) AS song_count 
+                                        FROM song AS s
+                                       GROUP BY s.album_id));
